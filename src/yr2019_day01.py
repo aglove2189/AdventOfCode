1bf4ec2f1da47c322c19dd7618df4dcb5d1bc9fb
Aaron Glover <aglove2189@gmail.com>
2019-12-01 22:50:32 -0600

    2019 day 1

---
+++ src/yr2019_day01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+
+
+data = get_data(2019, 1)
+
+def fuel(mass): 
+    return int(mass) // 3 - 2
+
+print(sum([fuel(d) for d in data.split()]))
+
+def fuel_total(mass):
+    f = fuel(mass)
+    return f + fuel_total(f) if f > 0 else 0
+
+print(sum([fuel_total(d) for d in data.split()]))
