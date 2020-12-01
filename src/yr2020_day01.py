4d896052117d096ccffef19bd2d9ddeaf720c7b8
Aaron Glover <aglove2189@gmail.com>
2020-11-30 23:31:38 -0600

    2020 day 1

---
+++ src/yr2020_day01.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import operator
+from itertools import combinations
+from functools import reduce
+
+from get_data import get_data
+
+
+data = get_data(2020, 1)
+data = list(map(int, data.split()))
+
+
+def func(data, r=2):
+    for c in combinations(data, r):
+        if sum(c) == 2020:
+            return reduce(operator.mul, c)
+
+
+print(func(data))
+print(func(data, r=3))
