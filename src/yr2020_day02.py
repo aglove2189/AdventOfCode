05b5f3ed0fe0a126efae5393b1f76276484451b4
Aaron Glover <aglove2189@gmail.com>
2020-12-01 23:42:24 -0600

    2020 day 2 update

--- src/yr2020_day02.py
+++ src/yr2020_day02.py
@@ -7,19 +7,10 @@ data = get_data(2020, 2)
 part1 = 0
 part2 = 0
 for d in data.splitlines():
-    policy, password = d.split(':')
-    password = password.strip()
+    min_max, char, password = d.split()
+    min_, max_ = (int(i) for i in min_max.split('-'))
 
-    min_, max_ = policy.split('-')
-    max_ = "".join(filter(str.isdigit, max_))
-    min_ = int(min_)
-    max_ = int(max_)
-
-    if min_ <= password.count(policy[-1]) <= max_:
-        part1 += 1
-
-    if (policy[-1] == password[min_-1]) != (policy[-1] == password[max_-1]):
-        part2 += 1
-
+    part1 += min_ <= password.count(char[0]) <= max_
+    part2 += (char[0] == password[min_-1]) != (char[0] == password[max_-1])
 print(part1)
 print(part2)
