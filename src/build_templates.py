6bc7b2acdb773756f25e33d75616aa8b75ed47dd
Aaron Glover <aglove2189@gmail.com>
2020-12-04 09:19:28 -0600

    better structure and template

--- src/build_templates.py
+++ src/build_templates.py
@@ -5,18 +5,19 @@ import click
 template = '''# -*- coding: utf-8 -*-
 from get_data import get_data\n
 
-data = get_data({year}, {day})'''
+data = get_data({yr}, {day})\n'''
 
 
 @click.command()
 @click.option('--year', '-yr', multiple=True)
 def build_templates(year):
     for yr in year:
+        os.makedirs(yr, exist_ok=True)
         for day in range(1, 26):
-            fn = f'yr{yr}_day{str(day).zfill(2)}.py'
+            fn = f'{yr}/day{str(day).zfill(2)}.py'
             if not os.path.exists(fn):
                 with open(fn, 'w') as f:
-                    f.write(template.format(year=yr, day=day))
+                    f.write(template.format(yr=yr, day=day))
 
 
 if __name__ == '__main__':
