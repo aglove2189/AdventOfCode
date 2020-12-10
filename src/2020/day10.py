6b4aa028d2d85d7f92142fba1ca82f2b34f89931
Aaron Glover <aglove2189@gmail.com>
2020-12-09 23:41:52 -0600

    2020 day 10

---
+++ src/2020/day10.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+import pandas as pd
+
+data = sorted(list(map(int, get_data(2020, 10).splitlines())))
+data.insert(0, 0)
+data.append(max(data) + 3)
+
+s = pd.Series(data).diff()
+print(len(s[s == 3]) * len(s[s == 1]))
+
+chains = [1] + [0] * (len(data) - 1)
+for i in range(1, len(chains)):
+    for j in range(0, i):
+        if data[i] - data[j] <= 3:
+            chains[i] += chains[j]
+print(max(chains))
