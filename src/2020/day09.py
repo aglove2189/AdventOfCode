1461e753c262f0005753334b4fbea1dd821732d3
Aaron Glover <aglove2189@gmail.com>
2020-12-09 21:38:41 -0600

    2020 day 9

---
+++ src/2020/day09.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+from itertools import combinations
+
+
+data = list(map(int, get_data(2020, 9).splitlines()))
+
+for i in range(25, len(data)):
+    if data[i] not in map(sum, combinations(data[i-25:i], 2)):
+        part1 = data[i]
+        break
+print(part1)
+
+for i in range(len(data)):
+    for j in range(i+2, len(data)):
+        s = data[i:j]
+        if sum(s) == part1:
+            part2 = min(s) + max(s)
+            break
+print(part2)
