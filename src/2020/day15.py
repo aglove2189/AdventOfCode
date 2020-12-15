edb9d7b2382bc2f5a88684878875cdbb2384811f
Aaron Glover <aglove2189@gmail.com>
2020-12-15 10:08:27 -0600

    2020 day 15

---
+++ src/2020/day15.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+
+
+data = list(map(int, get_data(2020, 15).split(',')))
+
+for num in (2020, 30000000):
+    seen, last = {e: i for i, e in enumerate(data, 1)}, data[-1]
+    for i in range(len(data), num):
+        seen[last], last = i, 0 if last not in seen else i-seen[last]
+    print(last)
