e60b72d881a4311aa421f753ad6a57a9571a6974
Aaron Glover <akglover@eprod.com>
2021-12-02 08:50:32 -0600

    2021 day 02

---
+++ src/2021/day02.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+
+
+data = get_data(2021, 2)
+data = [i.split(' ') for i in data.splitlines()]
+
+x, y, aim = 0, 0, 0
+for command, ammount in data:
+    if command == "up":
+        aim -= int(ammount)
+    elif command == "down":
+        aim += int(ammount)
+    elif command == "forward":
+        x += int(ammount)
+        y += aim * int(ammount)
+print(x * aim)
+print(x * y)
