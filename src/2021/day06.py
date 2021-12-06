04d895e90bd77e7403a72576c7827c2f9ac7b09f
Aaron Glover <akglover@eprod.com>
2021-12-06 08:27:20 -0600

    2021 day 6

---
+++ src/2021/day06.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+
+
+data = list(map(int, get_data(2021, 6).split(",")))
+
+
+def simulate(data, days=80):
+    fish = [data.count(i) for i in range(9)]
+    for _ in range(days):
+        new_fish = fish.pop(0)
+        fish[6] += new_fish
+        fish.append(new_fish)
+    return sum(fish)
+
+
+print(simulate(data))
+print(simulate(data, 256))
