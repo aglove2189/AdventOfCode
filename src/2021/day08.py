1c1c4e9f2b5521ff9cf7a41fa5376df88bf0c3d6
Aaron Glover <akglover@eprod.com>
2021-12-08 11:01:47 -0600

    2021 day 8 part 1

---
+++ src/2021/day08.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+
+
+data = get_data(2021, 8).splitlines()
+data = [d.split(' | ') for d in data]
+
+part1 = []
+for _, output in data:
+    part1.extend([i for i in list(map(len, output.split())) if i in [2, 4, 3, 7]])
+print(len(part1))
