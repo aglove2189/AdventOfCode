684f25a0450c4dc57fcad17eee00a623f7704a6d
Aaron Glover <akglover@eprod.com>
2021-12-09 13:26:35 -0600

    2021 day 9 - github copilot

--- src/2021/day09.py
+++ src/2021/day09.py
@@ -1,20 +1,54 @@
 # -*- coding: utf-8 -*-
+# Solutions were largely written by Github Copilot
 from get_data import get_data
-
+import numpy as np
 
 data = get_data(2021, 9).splitlines()
 data = [[int(y) for y in x] for x in data]
 
-part1 = 0
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        if i > 0 and data[i][j] >= data[i - 1][j]:
-            continue
-        if i < len(data) - 1 and data[i][j] >= data[i + 1][j]:
-            continue
-        if j > 0 and data[i][j] >= data[i][j - 1]:
-            continue
-        if j < len(data[0]) - 1 and data[i][j] >= data[i][j + 1]:
-            continue
-        part1 += data[i][j] + 1
-print(part1)
+
+def find_minimas(data):
+    minimas = []
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            left = data[i][j - 1] if j > 0 else 9
+            right = data[i][j + 1] if j < len(data[0]) - 1 else 9
+            above = data[i - 1][j] if i > 0 else 9
+            below = data[i + 1][j] if i < len(data) - 1 else 9
+            if (
+                data[i][j] < left
+                and data[i][j] < right
+                and data[i][j] < above
+                and data[i][j] < below
+            ):
+                minimas.append((i, j))
+    return minimas
+
+
+def flood_fill(data, x, y):
+    """
+    function with height map input of single digit numbers as array, second parameter is the
+    x and y coordinate of the starting point
+    flood fill outwards from starting point until digit 9 is reached
+    return the size of the filled area
+    """
+    size = 0
+    if data[x][y] == 9:
+        return 1
+    data[x][y] = 9
+    size = 1
+    if x > 0 and data[x - 1][y] != 9:
+        size += flood_fill(data, x - 1, y)
+    if x < len(data) - 1 and data[x + 1][y] != 9:
+        size += flood_fill(data, x + 1, y)
+    if y > 0 and data[x][y - 1] != 9:
+        size += flood_fill(data, x, y - 1)
+    if y < len(data[0]) - 1 and data[x][y + 1] != 9:
+        size += flood_fill(data, x, y + 1)
+    return size
+
+
+minimas = find_minimas(data)
+print(sum([data[i][j] for i, j in minimas]) + len(minimas))
+sizes = [flood_fill(data, x, y) for x, y in minimas]
+print(np.prod(sorted(sizes)[-3:]))
