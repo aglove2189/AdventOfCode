dc00e838512ae9e96ac3fc8e8a6dfef73aab765d
Aaron Glover <aglove2189@gmail.com>
2021-12-11 09:50:27 -0600

    2021 day 11

---
+++ src/2021/day11.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+import numpy as np
+
+
+data = get_data(2021, 11).splitlines()
+data = np.array([[int(y) for y in x] for x in data])
+
+part1, part2 = 0, 0
+while True:
+    part2 += 1
+    data += 1
+    flashing = np.where((data > 9) & (data < 420))
+    while len(flashing[0]) > 0:
+        coords = list(zip(flashing[0], flashing[1]))
+        for x, y in coords:
+            data[
+                max(0, x - 1):min(x + 2, data.shape[0]),
+                max(0, y - 1):min(y + 2, data.shape[1])
+            ] += 1
+            data[x, y] += 420
+            part1 += 1 if part2 <= 100 else 0
+        flashing = np.where((data > 9) & (data < 420))
+    data[data > 420] = 0
+    if data.sum() == 0:
+        print(part1, part2)
+        break
