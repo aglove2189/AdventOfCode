04dddf41feb18a114950b8ce5a32b908b25e899d
Aaron Glover <akglover@eprod.com>
2021-12-15 10:31:28 -0600

    2021 day 15

---
+++ src/2021/day15.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+from itertools import product
+import numpy as np
+import networkx as nx
+
+
+data = get_data(2021, 15).splitlines()
+data = [[int(y) for y in x] for x in data]
+
+
+def find_path(data):
+    n = len(data)
+    G = nx.grid_2d_graph(n, n, create_using=nx.DiGraph())
+    for i, j in G.edges:
+        G[i][j]['weight'] = data[j[1]][j[0]]
+    return nx.shortest_path_length(G, source=(0, 0), target=(n - 1, n - 1), weight='weight')
+
+
+print(find_path(data))
+
+n = len(data)
+data2 = np.zeros((n * 5, n * 5), dtype=np.uint8)
+for row, col in product(range(n * 5), range(n * 5)):
+    data2[col][row] = data[col % n][row % n] + (row // n) + (col // n)
+data2[data2 > 9] -= 9
+print(find_path(data2))
