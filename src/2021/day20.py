b756c0c8779b59931ab268125e42873cbaaca4bf
Aaron Glover <akglover@eprod.com>
2021-12-20 10:44:13 -0600

    2021 day 20

---
+++ src/2021/day20.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+import numpy as np
+from scipy.ndimage import convolve
+
+
+algo, _, *image = get_data(2021, 20).splitlines()
+algo = np.array([int(p == "#") for p in algo])
+image = np.pad([[int(p == "#") for p in row] for row in image], (51, 51))
+
+for i in range(1, 51):
+    image = algo[convolve(image, 2 ** np.arange(9).reshape(3, 3))]
+    if i in (2, 50):
+        print(image.sum())
