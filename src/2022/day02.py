beedc458ba66673aa26403406fe50b5ebd7985cc
Aaron Glover <aglove2189@gmail.com>
2022-12-02 09:26:14 -0600

    2022 day 2

---
+++ src/2022/day02.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+
+
+data = get_data(2022, 2)
+
+points = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
+}
+print(sum([points[d] for d in data.split("\n")]))
+
+points = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
+}
+print(sum([points[d] for d in data.split("\n")]))
