41c6d469d56357f328bdf58a6d4a9cb32b6ad7a1
Aaron Glover <aglove2189@gmail.com>
2022-12-03 08:09:37 -0600

    2022 day 3

---
+++ src/2022/day03.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+from string import ascii_letters
+
+data = get_data(2022, 3).split("\n")
+
+part1 = 0
+for rucksack in data:
+    split = len(rucksack) // 2
+    (letter,) = set(rucksack[split:]) & set(rucksack[:split])
+    part1 += ascii_letters.find(letter) + 1
+print(part1)
+
+part2 = 0
+for i in range(0, len(data), 3):
+    (letter,) = set.intersection(*map(set, data[i:i + 3]))
+    part2 += ascii_letters.find(letter) + 1
+print(part2)
