3cb97affcb7bce80b9a4d77b3bbb063679204ab1
Aaron Glover <aglove2189@gmail.com>
2022-12-03 23:33:29 -0600

    2022 day 4

---
+++ src/2022/day04.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from get_data import get_data
+import re
+
+data = get_data(2022, 4).split("\n")
+
+part1 = 0
+part2 = 0
+for i in data:
+    min1, max1, min2, max2 = list(map(int, re.findall(r"\d+", i)))
+
+    if min1 >= min2 and max1 <= max2:
+        part1 += 1
+    elif min2 >= min1 and max2 <= max1:
+        part1 += 1
+
+    if set(range(min1, max1 + 1)) & set(range(min2, max2 + 1)):
+        part2 += 1
+print(part1)
+print(part2)
