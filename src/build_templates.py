986790e46b9e2ec34bbb3d9272348d85f756069c
akglover <akglover@eprod.com>
2023-11-28 21:11:41 +0000

    bad path in build_templates

--- src/build_templates.py
+++ src/build_templates.py
@@ -8,9 +8,10 @@ data = get_data({year}, {day})\n"""
 
 if __name__ == "__main__":
     year = sys.argv[1]
-    os.makedirs(year, exist_ok=True)
+    fp = os.path.abspath(os.path.join(os.path.dirname(__file__), year))
+    os.makedirs(fp, exist_ok=True)
     for day in range(1, 26):
-        fn = f"{year}/day{day:02}.py"
+        fn = f"{fp}/day{day:02}.py"
         if not os.path.exists(fn):
             with open(fn, "w") as f:
                 f.write(template.format(year=year, day=day))
