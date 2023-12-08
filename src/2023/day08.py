a27ed314033bebe69717d964818944ef47b6e991
Aaron Glover <aglove2189@gmail.com>
2023-12-08 11:24:31 -0600

    code golfed

--- src/2023/day08.py
+++ src/2023/day08.py
@@ -8,21 +8,15 @@ data = get_data(2023, 8).splitlines()
 lr = namedtuple("lr", "L R")
 network = {d[0:3]: lr(d[7:10], d[12:15]) for d in data[2:]}
 
-p1 = 0
-current = "AAA"
-while current != "ZZZ":
-    for direction in data[0]:
-        p1 += 1
-        current = getattr(network[current], direction)
-print(p1)
 
-p2 = []
-for current in network:
-    if current.endswith("A"):
-        steps = 0
-        while not current.endswith("Z"):
-            for direction in data[0]:
-                steps += 1
-                current = getattr(network[current], direction)
-        p2.append(steps)
-print(math.lcm(*p2))
+def solve(start="AAA"):
+    steps = 0
+    while start != "ZZZ" if start == "AAA" else not start.endswith("Z"):
+        for direction in data[0]:
+            steps += 1
+            start = getattr(network[start], direction)
+    return steps
+
+
+print(solve())
+print(math.lcm(*(solve(n) for n in network if n.endswith("A"))))
